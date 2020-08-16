f43db1a2506fa30824ffc5a1fb769243ae52e6f0
Derek Fong <derek-fong@users.noreply.github.com>
2020-08-16 10:37:35 +0800

    feat(home): add HomePage content (#3)
    * Add HeroBanner. 
    * Add HomePage content. 
    * Add SEO default image.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,47 @@
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { NextPage } from 'next';
 import { NextSeoProps } from 'next-seo';
 import React, { ReactElement } from 'react';
 
+import HeroBanner from '../components/home/HeroBanner';
 import MainLayout from '../components/layouts/MainLayout';
 import { environment } from '../environment';
 
 const HomePage: NextPage = (): ReactElement => {
   const slogan = environment.app.slogan;
-  const title = 'Home';
+  const title = environment.app.name;
   const seoProps: NextSeoProps = {
     title,
     titleTemplate: `%s - ${slogan}`,
   };
 
   return (
-    <MainLayout content={{ style: { marginTop: '5em' } }} seoProps={seoProps}>
-      <h1>Home Page</h1>
+    <MainLayout content={{ style: { marginTop: '4em' } }} seoProps={seoProps}>
+      <div style={{ height: '100vh' }}>
+        <HeroBanner />
+        <div style={{ marginTop: '2em' }}>
+          <Container>
+            <Grid container spacing={4}>
+              <Grid item xs={12}>
+                <Typography component="p" variant="body1">
+                  <strong>{title}</strong> is a platform to recover from
+                  COVID-19 employment disruption by linking job seekers with
+                  businesses through short-term tasks to form new networks and
+                  generate references. Businesses benefit from reduction in
+                  hiring risk through references and economic work fulfilment.
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography align="center" component="p" variant="button">
+                  "Designed to be deleted"
+                </Typography>
+              </Grid>
+            </Grid>
+          </Container>
+        </div>
+      </div>
     </MainLayout>
   );
 };
