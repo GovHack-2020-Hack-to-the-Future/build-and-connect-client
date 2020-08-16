683c7ac012dea116a43d1844e66e772c59aa64c8
Derek Fong <derek.fong@alppix.com>
2020-08-16 13:03:34 +0800

    feat(users): add user profile content

--- src/pages/users/me.tsx
+++ src/pages/users/me.tsx
@@ -1,14 +1,27 @@
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import { NextPage } from 'next';
 import { NextSeoProps } from 'next-seo';
 import { useRouter } from 'next/router';
 import React, { ReactElement } from 'react';
 
 import MainLayout from '../../components/layouts/MainLayout';
+import QualificationsAccordion from '../../components/users/QualificationsAccordion';
+import RecentWorksAccordion from '../../components/users/RecentWorks';
 import { environment } from '../../environment';
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  avatarImg: {
+    width: '100%',
+  },
+});
 
 const MyProfilePage: NextPage = (): ReactElement => {
   const { asPath } = useRouter();
+  const classes = useStyles();
   const baseUrl = environment.baseUrl;
   const url = `${baseUrl}${asPath}`;
   const description = 'My Profile';
@@ -26,11 +39,46 @@ const MyProfilePage: NextPage = (): ReactElement => {
   };
 
   return (
-    <MainLayout content={{ style: { marginTop: '5em' } }} seoProps={seoProps}>
-      <h1>{`${title} Page`}</h1>
-      <Button color="primary" href="/api/auth/logout" variant="contained">
-        Logout
-      </Button>
+    <MainLayout
+      content={{ style: { marginTop: '6em' } }}
+      seoProps={seoProps}
+      topAppBar={{ backAs: '/jobs', backHref: '/jobs' }}
+    >
+      <Container>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Grid container justify="center">
+              <Grid item xs={4}>
+                <img
+                  className={classes.avatarImg}
+                  src="/images/default-avatar.png"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography align="center" component="h1" variant="h5">
+              Derek Fong
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <QualificationsAccordion />
+          </Grid>
+          <Grid item xs={12}>
+            <RecentWorksAccordion />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color="primary"
+              fullWidth
+              href="/api/auth/logout"
+              variant="contained"
+            >
+              Logout
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
     </MainLayout>
   );
 };
