1bd5f14212105888ebb6d25ca9ed934c271b3679
Derek Fong <derek-fong@users.noreply.github.com>
2020-08-16 14:46:48 +0800

    docs(readme): add quick start links (#7)
    - Add quick start links.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    runtimeCaching,
+    dest: 'public',
+  },
   target: 'serverless',
   webpack: (config) => {
     config.module.rules.push({
@@ -9,25 +16,4 @@ module.exports = {
 
     return config;
   },
-};
-
-// TODO: Uncomment to enable PWA.
-// const runtimeCaching = require('next-pwa/cache');
-// const withPWA = require('next-pwa');
-
-// module.exports = withPWA({
-//   pwa: {
-//     runtimeCaching,
-//     dest: 'public',
-//   },
-//   target: 'serverless',
-//   webpack: (config) => {
-//     config.module.rules.push({
-//       exclude: /node_modules/,
-//       loader: 'graphql-tag/loader',
-//       test: /\.graphql$/,
-//     });
-
-//     return config;
-//   },
-// });
+});
